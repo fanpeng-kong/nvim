a437b37cfb317d4029b49e020d7045e1891f0af6
Fanpeng Kong <fanpeng.kong@leapai.com.au>
2025-04-30 22:58:27 +1000

    update plugins

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -16,3 +16,12 @@ end
 vim.api.nvim_command([[
     autocmd ModeChanged * lua leave_snippet()
 ]])
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
+})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,5 +10,26 @@ return {
         },
       },
     },
+    optional = true,
+    specs = {
+      {
+        "ANGkeith/telescope-terraform-doc.nvim",
+        ft = { "terraform", "hcl" },
+        config = function()
+          LazyVim.on_load("telescope.nvim", function()
+            require("telescope").load_extension("terraform_doc")
+          end)
+        end,
+      },
+      {
+        "cappyzawa/telescope-terraform.nvim",
+        ft = { "terraform", "hcl" },
+        config = function()
+          LazyVim.on_load("telescope.nvim", function()
+            require("telescope").load_extension("terraform")
+          end)
+        end,
+      },
+    },
   },
 }
